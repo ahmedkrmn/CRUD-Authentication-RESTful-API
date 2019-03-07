bb677d9603aab39fe91aa4decdb17cfa882d821f
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-03-07 16:39:52 +0200

    Add authentication route

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+module.exports = (req, res, next) => {
+  let token = req.headers.authorization.split(" ")[1];
+  if (token) {
+    jwt.verify(token, config.JWT_SECRET, function(err) {
+      if (err) {
+        return res
+          .status(400)
+          .send("There is a problem with your access token, please sign in again!");
+      } else {
+        next();
+      }
+    });
+  } else {
+    return res.status(401).send("Please Sign in to continue.");
+  }
+};

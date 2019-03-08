45610107898af4cd5dd4054d748c73102f4888c2
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-03-08 10:24:20 +0200

    Add welcome page

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ mongoose.connection.on(
 );
 
 mongoose.connection.once("open", () => {
+  app.get("/", (req, res) => {
+    res.send(
+      '<h1>Welcome to the DJ/Producers API. Visit the <a href="https://github.com/ahmedkrmn/CRUD-Authentication-RESTful-API">Github</a> repo for instructions!</h1>'
+    );
+  });
   app.use("/user", user);
   app.use("/producer", producer);
   console.log(`Server started on ${config.PORT}`);

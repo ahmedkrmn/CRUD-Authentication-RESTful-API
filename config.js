817061f935bc86a63c2b0567c900677e501feb89
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-04-29 01:00:58 +0200

    Install redis

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || "http://localhost:3000",
-  DB_URI: process.env.DB_URI || "YOUR MONGODB ATLAS URI",
-  JWT_SECRET: process.env.JWT_SECRET || "development"
+  DB_URI: "DB URI",
+  JWT_SECRET: "JWT SECRET"
 };

a90695841ed9d405c4c9d697782eaf09b7d6c24e
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-04-29 01:09:31 +0200

    Update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || "http://localhost:3000",
-  DB_URI: "DB URI",
-  JWT_SECRET: "JWT SECRET"
+  DB_URI: process.env.DB_URI || "YOUR DB URI",
+  JWT_SECRET: process.env.JWT_SECRET || "YOUR JWT SECRET"
 };
